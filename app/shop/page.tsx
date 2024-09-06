de783a45237019394f1af5725b01168a83097ed3
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-09-07 06:09:09 +0700

    chore: add paragraph

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -69,7 +69,7 @@ const Shop = () => {
   return (
     <Layout>
       <Breadcrumb>
-        <BreadcrumbList className="mt-8 px-36">
+        <BreadcrumbList className="mt-2 p-4 md:px-36">
           <BreadcrumbItem>
             <BreadcrumbLink href="#">Beranda</BreadcrumbLink>
           </BreadcrumbItem>
@@ -84,17 +84,18 @@ const Shop = () => {
         </BreadcrumbList>
       </Breadcrumb>
 
-      <div className="container mx-auto p-4 px-36">
+      <div className="container mx-auto p-4 md:px-36">
         <div className="sticky top-[112px] z-50 w-full">
           <Button
             variant="default"
             size="lg"
-            className="mb-8 w-full rounded-lg bg-[#009EA9] px-4 py-2 text-white hover:bg-[#228d94]"
+            className="mb-4 w-full rounded-lg bg-[#009EA9] px-4 py-2 text-white hover:bg-[#228d94]"
             onClick={handleCompareClick}
           >
             Bandingkan Produk
           </Button>
         </div>
+        <p className="mb-8">Centang checkbox untuk membandingkan produk</p>
 
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
           {products.map((product) => (
